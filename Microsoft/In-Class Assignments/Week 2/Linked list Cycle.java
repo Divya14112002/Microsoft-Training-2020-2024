5c28da2696190f2e603af13961ff1e67598c8aba
Divya14112002 <112168351+Divya14112002@users.noreply.github.com>
2022-09-15 14:08:07 +0530

    Create Linked list Cycle.java

---
+++ Microsoft/In-Class Assignments/Week 2/Linked list Cycle.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fastPointer=head;
+        ListNode slowPointer=head;
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next.next;
+            if(slowPointer==fastPointer){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}

fe1cb3b5abab6cb15e4c5dee287237b44ce32975
Divya14112002 <112168351+Divya14112002@users.noreply.github.com>
2022-11-03 12:01:16 +0530

    Create Climbing Stairs.java

---
+++ Microsoft/In-Class Assignments/Week 9/Climbing Stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int climb[] = new int[n+1];
+        if(n==1){
+            return 1;
+        }
+        climb[1]=1;
+        climb[2]=2;
+        for(int i=3;i<=n;i++){
+            climb[i]=climb[i-1]+climb[i-2];
+        }
+        return climb[n];
+    }
+}

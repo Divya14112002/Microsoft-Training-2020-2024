f541a9442d63f70b282d05c06a272e0caaf66944
Divya14112002 <112168351+Divya14112002@users.noreply.github.com>
2022-11-03 11:08:32 +0530

    Update and rename Insert binary tree.java to Invert binary tree.java

---
+++ Microsoft/In-Class Assignments/Week 4/Invert binary tree.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return root;
+        }
+        invert (root);
+        return root;
+    }
+    public void invert(TreeNode root){
+        if(root==null){
+            return;
+        }
+        TreeNode holder=root.left;
+        root.left=root.right;
+        root.right=holder;
+        invert(root.left);
+        invert(root.right);
+    }
+}

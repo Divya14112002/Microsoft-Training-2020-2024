3de0c02e28d57de5e5f2ca4857d02f9c0a1d2f94
Divya14112002 <112168351+Divya14112002@users.noreply.github.com>
2022-11-03 11:56:07 +0530

    Create Pow(x,n).java

---
+++ Microsoft/In-Class Assignments/Week 8/Pow(x,n).java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            x=1/x;
+            n=n*(-1);
+        }
+       return helper(x,n);
+    }
+    public double helper(double x,int n){
+        if(n==0){
+            return 1;
+        }
+        if(n%2==0){
+           return helper(x*x,n/2);
+        }else{
+            return x*helper(x*x,n/2);
+        }
+    }
+}
+Footer
+© 2022 GitHub, Inc.

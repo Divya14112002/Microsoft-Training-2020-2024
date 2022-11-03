fe8cc72074c35a89a0ed7867c52809ce4e8865a0
Divya14112002 <112168351+Divya14112002@users.noreply.github.com>
2022-11-03 11:13:49 +0530

    Update Implement DFS.java

--- Microsoft/In-Class Assignments/Week 6/Implement DFS.java
+++ Microsoft/In-Class Assignments/Week 6/Implement DFS.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited=new boolean[V];
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                dfs(visited,adj,i,res);
+            }
+        }
+        return res;
+    }
+    public void dfs(boolean[] visited,ArrayList<ArrayList<Integer>> adj,int node,ArrayList<Integer> res){
+        visited[node]=true;
+        res.add(node);
+        for(int i:adj.get(node)){
+            if(!visited[i]){
+                dfs(visited,adj,i,res);
+            }
+        }
+    }
+}

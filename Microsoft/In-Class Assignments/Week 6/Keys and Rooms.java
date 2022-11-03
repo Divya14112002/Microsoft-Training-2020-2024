0b63c71d1821a75b150a002c34fa67807eb20c9c
Divya14112002 <112168351+Divya14112002@users.noreply.github.com>
2022-11-03 11:15:03 +0530

    Update Keys and Rooms.java

--- Microsoft/In-Class Assignments/Week 6/Keys and Rooms.java
+++ Microsoft/In-Class Assignments/Week 6/Keys and Rooms.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n=rooms.size();
+        boolean[] visited=new boolean[n];
+        Queue<Integer> q=new LinkedList<>();
+        q.add(0);
+        visited[0]=true;
+        while(!q.isEmpty()){
+            int s=q.poll();
+            for(int i=0;i<rooms.get(s).size();i++){
+                int k=rooms.get(s).get(i);
+                if(!visited[k]){
+                    visited[k]=true;
+                    q.add(k);
+                }
+            }
+        }
+        for(boolean i:visited){
+            if(!i){
+                return false;
+            }
+        }
+        return true;
+    }
+}
